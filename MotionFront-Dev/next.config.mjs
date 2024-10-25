17b46d278d3276e92f76401c41a9fc811689410d
JhoanDuarte <46505335+JhoanDuarte@users.noreply.github.com>
2024-10-24 21:30:12 -0500

    Update next.config.mjs

--- MotionFront-Dev/next.config.mjs
+++ MotionFront-Dev/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+
+  // Aquí se agregan las variables de entorno
+  env: {
+    API_URL: process.env.API_URL || 'https://backend-e5yd.onrender.com', // URL de tu backend
+  },
+
+  reactStrictMode: true,
+};
 
 export default nextConfig;
