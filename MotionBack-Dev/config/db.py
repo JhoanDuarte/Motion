daa3f3aa46ab9d57f2deda55efbb8a5418d0f3c7
JhoanDuarte <46505335+JhoanDuarte@users.noreply.github.com>
2024-10-24 19:24:41 -0500

    Update db.py

--- MotionBack-Dev/config/db.py
+++ MotionBack-Dev/config/db.py
@@ -7,12 +7,14 @@ load_dotenv()
 db_user = os.getenv('DB_USER')
 db_pass = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 
 conn = mysql.connector.connect(
     user=db_user,
     password=db_pass,
     host=db_host,
+    port=db_port,
     database=db_name,
 )
 
