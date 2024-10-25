6734f96f329d5c0f5bd17434ff13449e03bfec0c
JhoanDuarte <46505335+JhoanDuarte@users.noreply.github.com>
2024-10-24 22:23:09 -0500

    Update motionController.py

--- MotionBack-Dev/controller/motionController.py
+++ MotionBack-Dev/controller/motionController.py
@@ -1,18 +1,20 @@
+from model.motionModel import RecordModel
+
 class RecordController:
     def __init__(self):
         self.model = RecordModel()
 
-    def getAll(self, conn):
-        return self.model.getAll(conn)
+    def getAll(self):
+        return self.model.getAll()
     
-    def getById(self, conn, id):
-        return self.model.getById(conn, id)
+    def getById(self, id):
+        return self.model.getById(id)
     
-    def create(self, conn, brand, location, candidate):
-        return self.model.create(conn, brand, location, candidate)
+    def create(self, brand, location, candidate):
+        return self.model.create(brand, location, candidate)
 
-    def update(self, conn, id, brand, location, candidate):
-        return self.model.update(conn, id, brand, location, candidate) > 0
+    def update(self, id, brand, location, candidate):
+        return self.model.update(id, brand, location, candidate) > 0
 
-    def delete(self, conn, id):
-        return self.model.delete(conn, id) > 0
+    def delete(self, id):
+        return self.model.delete(id) > 0
