798174f636867ee3ed0f116cb064f124073d76b4
JhoanDuarte <46505335+JhoanDuarte@users.noreply.github.com>
2024-10-24 22:59:40 -0500

    Update motionController.py

--- MotionBack-Dev/controller/motionController.py
+++ MotionBack-Dev/controller/motionController.py
@@ -10,13 +10,13 @@ class RecordController:
     def getById(self, cursor, id):
         return self.model.getById(cursor, id)
     
-    def create(self, cursor, brand, location, candidate):
+    def create(self, brand, location, candidate):
         cursor.execute("INSERT INTO records (brand, location, candidate) VALUES (%s, %s, %s)", (brand, location, candidate))
         cursor.connection.commit()  # Cambiar a cursor.connection.commit()
         return cursor.lastrowid
 
-    def update(self, cursor, id, brand, location, candidate):
+    def update(self, id, brand, location, candidate):
         return self.model.update(cursor, id, brand, location, candidate) > 0
 
-    def delete(self, cursor, id):
+    def delete(self, id):
         return self.model.delete(cursor, id) > 0
