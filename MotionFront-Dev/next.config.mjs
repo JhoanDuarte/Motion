20183da854aa4a6918705eef0e847033078af903
JhoanDuarte <46505335+JhoanDuarte@users.noreply.github.com>
2024-10-24 21:52:11 -0500

    Update next.config.mjs

--- MotionFront-Dev/next.config.mjs
+++ MotionFront-Dev/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Asegúrate de que esto esté configurado para exportar
+  output: 'export', // Esta línea es correcta
 };
 
 export default nextConfig;
