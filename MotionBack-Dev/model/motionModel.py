55d9928650aa806fedc2258995a88a64200a7edf
JhoanDuarte <46505335+JhoanDuarte@users.noreply.github.com>
2024-10-24 22:19:51 -0500

    Update motionModel.py

--- MotionBack-Dev/model/motionModel.py
+++ MotionBack-Dev/model/motionModel.py
@@ -1,29 +1,29 @@
 from config.db import conn, cursor
 
 class RecordModel:
-    def __init__(self):
+    def __init__(self, conn):
         self.conn = conn
         self.cursor = cursor
 
-    def getAll(self):
+     def getAll(self, conn):
         self.cursor.execute("SELECT * FROM records")
         return self.cursor.fetchall()
 
-    def getById(self, id):
+    def getById(self, id, conn):
         self.cursor.execute("SELECT * FROM records WHERE id = %s", (id,))
         return self.cursor.fetchone()
 
-    def create(self, brand, location, candidate):
+    def create(self, brand, location, candidate, conn):
         self.cursor.execute("INSERT INTO records (brand, location, candidate) VALUES (%s, %s, %s)", (brand, location, candidate))
         self.conn.commit()
         return self.cursor.lastrowid
 
-    def update(self, id, brand, location, candidate):
+    def update(self, id, brand, location, candidate, conn):
         self.cursor.execute("UPDATE records SET brand = %s, location = %s, candidate = %s WHERE id = %s", (brand, location, candidate, id))
         self.conn.commit()
         return self.cursor.rowcount
 
-    def delete(self, id):
+    def delete(self, id, conn):
         self.cursor.execute("DELETE FROM records WHERE id = %s", (id,))
         self.conn.commit()
         return self.cursor.rowcount
