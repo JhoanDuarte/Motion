f8d81cdd4cb4ca196405fbec81972db7adb6de30
JhoanDuarte <46505335+JhoanDuarte@users.noreply.github.com>
2024-10-24 22:34:41 -0500

    Update motionController.py

--- MotionBack-Dev/controller/motionController.py
+++ MotionBack-Dev/controller/motionController.py
@@ -10,20 +10,10 @@ class RecordController:
     def getById(self, cursor, id):
         return self.model.getById(cursor, id)
     
-        @app.post("/records", response_model=dict)
-        async def create(record: RecordCreate):
-            conn = get_db_connection()
-            cursor = conn.cursor()  # Crear un cursor
-            try:
-                # Asegúrate de capturar el ID devuelto si es necesario
-                record_id = controller.create(cursor, record.brand, record.location, record.candidate)
-                return {"message": "Registro creado exitosamente", "id": record_id}
-            except Exception as e:
-                raise HTTPException(status_code=500, detail=f"Error al crear el registro: {e}")
-            finally:
-                cursor.close()  # Cerrar el cursor
-                conn.close()  # Cerrar la conexión
-
+    def create(self, cursor, brand, location, candidate):
+        cursor.execute("INSERT INTO records (brand, location, candidate) VALUES (%s, %s, %s)", (brand, location, candidate))
+        cursor.connection.commit()  # Cambiar a cursor.connection.commit()
+        return cursor.lastrowid
 
     def update(self, cursor, id, brand, location, candidate):
         return self.model.update(cursor, id, brand, location, candidate) > 0
